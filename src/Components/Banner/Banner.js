0b3c479e765ea1c26c74a54c50976e673a094af4
EdebaliErman <edebalierman@gmail.com>
2023-11-14 17:00:45 +0300

    Banner p düzenlendi

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,7 +5,7 @@ function Banner() {
     return (
         <div className='w-1/2 flex flex-col gap-3'>
             <h1 className='text-6xl pr-96 text-main font-medium font-sans text-left'>{page.header.head}</h1>
-            <p className='text-3xl pr-6 mt-3 text-left text-main opacity-90 font-normal font-sans'>{page.header.headParagraph}</p>
+            <p className='text-2xl pr-[12rem] mt-3 text-left text-main opacity-90 font-normal font-sans'>{page.header.headParagraph}</p>
         </div>
     )
 }
