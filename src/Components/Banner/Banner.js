281a2586c64704ab0691c9bc35fb629d23a2d044
EdebaliErman <edebalierman@gmail.com>
2023-11-14 16:52:56 +0300

    Banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -3,9 +3,9 @@ import { page } from '../../Data/data'
 
 function Banner() {
     return (
-        <div>
-            <h1>{page.header.head}</h1>
-            <p>{page.header.headParagraph}</p>
+        <div className='w-1/2 flex flex-col gap-3'>
+            <h1 className='text-6xl pr-96 text-main font-medium font-sans text-left'>{page.header.head}</h1>
+            <p className='text-3xl pr-6 mt-3 text-left text-main opacity-90 font-normal font-sans'>{page.header.headParagraph}</p>
         </div>
     )
 }
